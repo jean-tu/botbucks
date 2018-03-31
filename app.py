cb94c12a6c33db0e0f736accc41622db0107d7d6
harleyhacker <a777juarez@gmail.com>
2018-03-31 16:15:28 -0700

    Major UI changes. Integrated chatbot iFrame into HTML structure Created new pages.

--- app.py
+++ app.py
@@ -6,7 +6,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index(): 
-	return render_template ('INDEX.html')
+	return render_template ('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/lesson1')
+def lesson1():
+    return render_template('lesson1.html')
 
 @app.route('/shutdown', methods=['POST'])
 def shutdown():
