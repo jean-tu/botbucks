0baba3f6b60362aaed9ce4379d4a278b2dc2b4e0
jeanietwo <tujean27@gmail.com>
2018-04-01 07:07:45 +0000

    pushing the working appspot

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask import render_template
 from flask import request, make_response # in order to be able to use the webhook
-from scripts.cinterest import compute_comp_int
-from google.cloud import bigquery 
+#from scripts.cinterest import compute_comp_int
+#from google.cloud import bigquery 
 import json
 
 app = Flask(__name__)
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index(): 
-	return render_template ('index.html')
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
@@ -24,17 +24,17 @@ def lesson1():
 #Provie the user information on the stock that they are requesting information on 
 @app.route('/compute', methods=['POST'])
 def compute():
-	print("HELLO INSIDE COMPUTE") #debug
-	coffeeprice = "This is a response from GAE !!!"
-	my_response = {
-		"coffee-price" : coffeeprice, 
-		"size": "large", 
-		"drink": "latte",
-	}
-	res = json.dump(my_response)
-	r = make_response(res) 
-	r.headers['Content-Type'] = 'application/json'
-	return r 
+    print("HELLO INSIDE COMPUTE") #debug
+    coffeeprice = "This is a response from GAE !!!"
+    my_response = {
+	"coffee-price" : coffeeprice, 
+	"size": "large", 
+	"drink": "latte",
+    }
+    res = json.dump(my_response)
+    r = make_response(res) 
+    r.headers['Content-Type'] = 'application/json'
+    return r 
 
 
 @app.route('/shutdown', methods=['POST'])
@@ -44,5 +44,5 @@ def shutdown():
 
 
 
-if __name__ == '__main__': 
-	app.run(debug = True)
+#if __name__ == '__main__': 
+#	app.run(debug = True)
