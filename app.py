b4124a880a93ea99cf9b549e1699678e1285606a
uscitp-jeantu <tujean27@gmail.com>
2018-04-01 09:31:38 -0700

    test make response

--- app.py
+++ app.py
@@ -26,19 +26,22 @@ def lesson1():
 #Provie the user information on the stock that they are requesting information on
 @app.route('/compute', methods=['POST'])
 def compute():
-    req = request.get_json(silent=True, force=True)
-    stock = req['result']['parameters'].get('stockName')
-    # createPortfolio(1000, [stock])#calls on the function from porfoliomaker
-    # print("HELLO INSIDE COMPUTER ", stock, file=sys.stdout) #debug
-    my_response = {
-	   "speech": "hello",
-       "diaplayText": "hello",
-    }
-    res = json.dump(my_response)
-    r = make_response(res)
-    r.headers['Content-Type'] = 'application/json'
-    print(r)
+    # req = request.get_json(silent=True, force=True)
+    # stock = req['result']['parameters'].get('stockName')
+    # # createPortfolio(1000, [stock])#calls on the function from porfoliomaker
+    # print(stock) #debug
+    # my_response = {
+       # "speech": "hello",
+    #    "diaplayText": "hello",
+    # }
+    # res = json.dump(my_response)
+    # r = make_response(res)
+    # r.headers['Content-Type'] = 'application/json'
+    # print(r)
+    # return r
+    r = make_response()
     return r
+
 
 
 @app.route('/shutdown', methods=['POST'])
